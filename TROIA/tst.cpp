ce0ef7cbbd66761af683ab877e4415b5b25cbe3f
luissfernann <luisfernandomagnum@gmail.com>
2024-12-20 17:43:30 -0300

    main save

---
+++ TROIA/tst.cpp
@@ -0,0 +1,18 @@
+// PROTOTYPE : jogo da velha 
+    // tabuleiro : 9 spaces
+        // plays : 9 spaces 
+        // victory min : 5 spaces 
+    // if ( sequence : victory ) else { tie (return) } 
+    // players : 2 
+            // names 
+            // space 1 ( player 1 ) . space 2 ( player 2 ) . space 3 ( player 1 ), space 4 ( player 2 )...
+                // if (player 1 == x ) { if ( pl 1 ! = x) return falase } else { return } } 
+
+// FALTA: 
+    // preencher o restante do vetor dos nome, com \0;
+    // ao dar espacos depois de escolheu o nome, o cinignore não funciona 
+
+// FEITO 
+    // exige nome1, nome2
+    // ( ( nome1 = X v O ) -> ( nome2 == X v O )) .
+    // exibe : nome + tipo escolhido
